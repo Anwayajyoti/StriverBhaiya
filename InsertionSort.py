58d03824fc1680daf00ba8cef4a33388b046ee20
Anwayajyoti <69097306+Anwayajyoti@users.noreply.github.com>
2023-12-25 01:08:57 +0530

    Update InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -11,7 +11,7 @@ for i in range(1,n):
     while j>=0 and arr[j]>temp:
         arr[j+1]=arr[j]
         j-=1
-    arr[j+1]=temp
+        arr[j+1]=temp
 
 for i in range(n):
     print(arr[i],end=' ')
