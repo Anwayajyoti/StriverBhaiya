07374f78f5c0f050860b91655c11eada4cb0399e
Anwayajyoti <69097306+Anwayajyoti@users.noreply.github.com>
2023-12-31 20:38:12 +0530

    Update TimeConversion.py

--- TimeConversion.py
+++ TimeConversion.py
@@ -1,13 +1,19 @@
-s='12:00:00AM'
+s1='12:00:00AM'
+s2='01:00:00AM'
+s3='12:00:00PM'
+s4='11:59:59PM'
 def timeConversion(s):
-    # Write your code here
-    if s[-2:] == "AM" and s[:2] == "12": 
-        return "00" + s[2:-2] 
-    elif s[-2:] == "AM": 
-        return s[:-2]
-    elif s[-2:] == "PM" and s[:2] == "12": 
-        return s[:-2] 
+    if s[-2:]=='AM' and s[:2]=='12':
+        return('00'+s[2:-2])
+    elif s[-2:]=='AM':
+        return(s[0:-2])
+    elif s[-2:]=='PM' and s[:2]=='12':
+        return('12'+s[2:-2])
     else:
-       ans = int(s[:2]) + 12
-       return str(str(ans) + s[2:8]) 
-print(timeConversion(s))
+        return(str(12+int(s[:2]))+s[2:-2])
+print(timeConversion(s1))
+print(timeConversion(s2))
+print(timeConversion(s3))
+print(timeConversion(s4))
+
+
